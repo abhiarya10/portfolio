0c172b62b4b01b7907b8d62437f4a03cfb266eea
Abhigyan <abhi2018aray@gmail.com>
2024-03-04 00:34:15 +0530

    worked on contact form like responsive and dark mode

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Contact.css";
 import mailLogo from "../Assests/Icons/mailLogo.png";
+import { darkModeContext } from "../globalState/globalDarkMode";
 
 function Contact({ forwardRef }) {
+  const { globalDark } = useContext(darkModeContext);
+
   return (
     <div className="contact-div" ref={forwardRef}>
-      <p>Reach out to me</p>
-      <div className="contact-container">
+      <p className={`${globalDark ? "dark-contact-header" : ""}`}>
+        Reach out to me
+      </p>
+      <div
+        className={`${
+          globalDark ? "dark-contact-container" : "contact-container"
+        }`}
+      >
         <div className="mail-div">
           <img src={mailLogo} />
           <a href="mailto:abhi2018arya@gmail.com" className="gmail-link">
-            Contact with gmail
+            Contact via Email
           </a>
         </div>
         <p className="or-text">or</p>
-        <div className="contact-form">
+        <div className={`${globalDark ? "dark-contact-form" : "contact-form"}`}>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="text" placeholder="Message" />
-          <button className="contact-sub-btn">Submit</button>
+          <button
+            className={`${
+              globalDark ? "dark-contact-sub-btn" : "contact-sub-btn"
+            }`}
+            onClick={() => alert("Please use ' Contact via Email ' option")}
+          >
+            Submit
+          </button>
         </div>
       </div>
     </div>
