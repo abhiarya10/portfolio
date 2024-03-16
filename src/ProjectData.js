b5c1a91ef4f58fdba996ad1d22dd40270548d8c1
Abhigyan <abhi2018aray@gmail.com>
2024-03-16 11:45:30 +0530

    added chatbury project

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,4 +1,5 @@
 import melodifyImage from "./Assests/melodifyImage.jpg";
+import chatburyImage from "./Assests/chatburyImage.jpg";
 
 const projectList = [
   {
@@ -9,6 +10,15 @@ const projectList = [
     techUsed: "React js, Express js, MongoDB",
     projectDesc:
       "Melodify provides a dynamic and responsive music streaming experience, enabling users to add songs to their favorite playlists and seamlessly enjoy their favorite tunes across all devices.",
+  },
+  {
+    projectID: 2,
+    projectTitle: "Chatbury",
+    projectImage: chatburyImage,
+    projectLink: "https://chatbury.vercel.app/",
+    techUsed: "React js, Socket.io, MongoDB",
+    projectDesc:
+      "Chatbury is a real-time chat application powered by MERN stack and Socket.IO. It enables users to engage in instant conversations and view their message history, facilitating seamless communication in a user-friendly interface.",
   },
 ];
 
